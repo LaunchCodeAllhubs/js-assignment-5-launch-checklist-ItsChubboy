28c308d112d6b65229080899cf184633459a7885
Colin Robinson <colin@Colins-Air.lan>
2023-08-09 15:24:30 -0500

    Finished script helper and working through script.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,37 +3,81 @@ require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
-   /*
+   let missionTarget = document.getElementById("missionTarget");
+   missionTarget.innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">
+`
 }
 
 function validateInput(testInput) {
-   
+    if (testInput === '') {
+        return "Empty";
+    } else if (isNaN(testInput)) {
+        return "Not a Number";
+    } else if (!isNaN(testInput)) {
+        return "Is a Number";
+    }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+   if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+    alert("Invalid input!");
+   } else if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+    alert("Please enter inputs into all fields!");
+   } else {
+    list.style.visibility = "visible";
+    let pilotStatus = document.getElementById("pilotStatus");
+    let copilotStatus = document.getElementById("copilotStatus");
+    let fuelStatus = document.getElementById("fuelStatus");
+    let cargoStatus = document.getElementById("cargoStatus");
+    let launchStatus = document.getElementById("launchStatus");
+    pilotStatus.innerHTML = `${pilot} is ready for launch`;
+    copilotStatus.innerHTML = `${copilot} is ready for launch`;
+    if (fuelLevel < 10000 && cargoLevel <= 10000) {
+        fuelStatus.innerHTML = `Fuel level is not high enough for launch`;
+        cargoStatus.innerHTML = `Cargo mass low enough for launch`;
+        launchStatus.innerHTML = `Shuttle not ready for launch`;
+        launchStatus.style.color = "#C7254E";
+    } else if (fuelLevel < 10000 && cargoLevel > 10000) {
+        fuelStatus.innerHTML = `Fuel level is not high enough for launch`;
+        cargoStatus.innerHTML = `Cargo mass too high for launch`;
+        launchStatus.innerHTML = `Shuttle not ready for launch`;
+        launchStatus.style.color = "#C7254E";
+    } else if (cargoLevel >= 10000 && fuelLevel > 10000) {
+        fuelStatus.innerHTML = `Fuel level high enough for launch`;
+        cargoStatus.innerHTML = `Cargo mass too high for launch`;
+        launchStatus.innerHTML = `Shuttle not ready for launch`;
+        launchStatus.style.color = "#C7254E";
+    } else {
+        fuelStatus.innerHTML = `Fuel level high enough for launch`;
+        cargoStatus.innerHTML = `Cargo mass low enough for launch`;
+        launchStatus.innerHTML = `Shuttle ready for launch`;
+        launchStatus.style.color = "#419F6A";
+    }
+   };
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        return response.json();
         });
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    let planetIndex = Math.floor(Math.random() * planets.length);
+    return planets[planetIndex];
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
